d987d592ad0e2634d38484c49bbaae33f64e3554
Jacob Marshall <me@jhqcat.com>
2024-02-29 16:16:12 +0000

    moveable paddles

--- game.py
+++ game.py
@@ -18,6 +18,7 @@ ball_vel_x, ball_vel_y = 0.7, 0.7
 paddle_width, paddle_height = 20, 120
 left_paddle_y = right_paddle_y = HEIGHT/2 - paddle_height
 left_paddle_x, right_paddle_x = 100 - paddle_width/2, WIDTH - (100 - paddle_width/2)
+right_paddle_vel = left_paddle_vel = 0
 
 while run:
     wn.fill(BLACK)
@@ -25,6 +26,18 @@ while run:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             run = False
+        elif i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_UP:
+                right_paddle_vel = -0.9
+            if i.key == pygame.K_DOWN:
+                right_paddle_vel = 0.9
+            if i.key == pygame.K_w:
+                left_paddle_vel =-0.9
+            if i.key == pygame.K_s:
+                left_paddle_vel =0.9
+        if i.type == pygame.KEYUP:
+            right_paddle_vel = 0
+            left_paddle_vel = 0
 
     if ball_y <= 0 + radius or ball_y >= HEIGHT - radius:
         ball_vel_y *= -1
@@ -38,6 +51,17 @@ while run:
 
     ball_x += ball_vel_x
     ball_y += ball_vel_y
+    right_paddle_y += right_paddle_vel
+    left_paddle_y += left_paddle_vel
+
+    if left_paddle_y >= HEIGHT - paddle_height:
+        left_paddle_y = HEIGHT - paddle_height
+    if left_paddle_y <= 0:
+        left_paddle_y = 0
+    if right_paddle_y >= HEIGHT - paddle_height:
+        right_paddle_y = HEIGHT - paddle_height
+    if right_paddle_y <= 0:
+        right_paddle_y = 0
 
     pygame.draw.circle(wn, BLUE, (ball_x, ball_y), radius)
     pygame.draw.rect(wn, RED, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
