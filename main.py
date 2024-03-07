78e48f46fc702a98fe8dbd862d47087e2db25347
Jacob Marshall <me@jhqcat.com>
2024-03-07 10:34:20 +0000

    add movemrnt and log score to the console

--- main.py
+++ main.py
@@ -12,14 +12,24 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 game = Game(window, WIDTH, HEIGHT)
 
 run = True
+clock = pygame.time.Clock()
 while run:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
     
-    game.loop()
-    game.draw()
+    # movement for human (left side only)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        game.move_paddle(left=True, up=True)
+    if keys[pygame.K_s]:
+        game.move_paddle(left=True, up=False)
+    
+    game_info = game.loop()
+    print(game_info.left_score, game_info.right_score)
+    game.draw(False, True)
     pygame.display.update()
     
 pygame.quit()
