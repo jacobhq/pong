d81f89a8aa8ad272014777c120f1854c6a3da86d
Jacob Marshall <me@jhqcat.com>
2024-03-07 10:56:42 +0000

    refactor into a class

--- main.py
+++ main.py
@@ -6,30 +6,33 @@ import os
 import time
 import pickle
 
-WIDTH, HEIGHT = 700, 500
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-
-game = Game(window, WIDTH, HEIGHT)
-
-run = True
-clock = pygame.time.Clock()
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
-    
-    # movement for human (left side only)
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        game.move_paddle(left=True, up=True)
-    if keys[pygame.K_s]:
-        game.move_paddle(left=True, up=False)
-    
-    game_info = game.loop()
-    print(game_info.left_score, game_info.right_score)
-    game.draw(False, True)
-    pygame.display.update()
+class PongGame:
+    def __init__(self, window, width, height):
+        self.game = Game(window, width, height)
+        self.left_paddle = self.game.left_paddle
+        self.right_paddle = self.game.right_paddle
+        self.ball = self.game.ball
+        
+    def test_ai(self):
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
+            
+            # movement for human (left side only)
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                self.game.move_paddle(left=True, up=True)
+            if keys[pygame.K_s]:
+                self.game.move_paddle(left=True, up=False)
+            
+            game_info = self.game.loop()
+            print(game_info.left_score, game_info.right_score)
+            self.game.draw(False, True)
+            pygame.display.update()
     
 pygame.quit()
