1c6dffd2ac734e24adb1c1a9fc2832628169176c
Jacob Marshall <me@jhqcat.com>
2024-04-07 15:39:11 +0100

    fix select (again)

--- web/components/game-builder.tsx
+++ web/components/game-builder.tsx
@@ -21,7 +21,7 @@ export function GameBuilder() {
           <div className="space-y-2">
             <div className="space-y-2">
               <Label htmlFor="model">Model</Label>
-              <Select defaultValue="@jhqcat/pong-v0.1.0" id="model">
+              <Select defaultValue="@jhqcat/pong-v0.1.0">
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Select a model" />
                 </SelectTrigger>
