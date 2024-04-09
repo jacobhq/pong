af2c4937c5e8683baf03e041ca1941274f7b7e35
Jacob Marshall <me@jhqcat.com>
2024-04-09 20:29:03 +0100

    wip add a marketing page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,9 @@
-import {Leaderboard} from "@/components/leaderboard";
+import {MarketingHero} from "@/components/marketing-hero";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <Leaderboard />
+      <MarketingHero />
     </main>
   );
 }
