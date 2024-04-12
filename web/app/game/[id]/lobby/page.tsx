a8cf3c8f73381baef0693470031459c4f6e9b9a1
Jacob Marshall <me@jhqcat.com>
2024-04-12 13:47:45 +0100

    fix lobby auth

--- web/app/game/[id]/lobby/page.tsx
+++ web/app/game/[id]/lobby/page.tsx
@@ -1,12 +1,11 @@
 import {Lobby} from "@/components/lobby";
-import {getServerSession} from "next-auth";
-import {authOptions} from "@/lib/auth";
+import {auth} from "@/lib/auth";
 import {getGame} from "@/lib/queries";
 import {notFound} from "next/navigation";
 
 export default async function Page({params}: { params: { id: string } }) {
-    const session = (await getServerSession(authOptions)) || {};
-    const game = await getGame(params.id, session.user.id as string);
+    const session = await auth()
+    const game = await getGame(params.id, session?.user?.id as string);
 
     if (!game) return notFound()
 
