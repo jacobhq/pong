1ecb38b16e653b0d2c99db50bc5b17168f052650
Jacob Marshall <me@jhqcat.com>
2024-04-13 16:05:13 +0100

    fix auth in middleware

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,13 +1,3 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export default async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname;
-  const session = !!req.cookies.get("next-auth.session-token")
-
-  if (!session) {
-    return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${path}`, req.url));
-  }
-  return NextResponse.next();
-}
+export { auth as middleware } from "@/lib/auth"
 
 export const config = { matcher: ["/game/:path*", "/api/game/:path*"] }
