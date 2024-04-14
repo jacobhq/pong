4ac2dbbfaffeca451b4145d749c87431311be100
Jacob Marshall <me@jhqcat.com>
2024-04-14 21:40:54 +0000

    start ingest work

--- web/app/ingest/[id]/join/route.ts
+++ web/app/ingest/[id]/join/route.ts
@@ -1,9 +1,9 @@
-import {getGame, getPlayer, newPlayer} from "@/lib/queries";
-import {notFound} from "next/navigation";
-import {JoinRes, Player} from "@/lib/types";
+import { getGame, getModelUrl, newPlayer } from "@/lib/queries";
+import { notFound } from "next/navigation";
+import { JoinRes } from "@/lib/types";
 
 export const dynamic = 'force-dynamic' // defaults to auto
-export async function POST(request: Request, {params}: { params: { id: string } }) {
+export async function POST(request: Request, { params }: { params: { id: string } }) {
     const body = await request.json()
     const game = await getGame(params.id, undefined, true)
     if (!game) return notFound()
@@ -11,13 +11,14 @@ export async function POST(request: Request, {params}: { params: { id: string } 
         status: 400
     })
 
-    const createdPlayer = await newPlayer(params.id, body.displayName)
-    const player = await getPlayer(createdPlayer) as Player
+    const playerId = await newPlayer(params.id, body.displayName)
+
+    const modelUrl = await getModelUrl(game.modelName) as string
 
     const res: JoinRes = {
         gameId: params.id,
-        playerId: player.id,
-        modelUrl: game.model
+        playerId: playerId,
+        modelUrl: modelUrl
     }
 
     return new Response(JSON.stringify(res))
--- web/app/ingest/[id]/route.ts
+++ web/app/ingest/[id]/route.ts
@@ -1,13 +1,13 @@
-import {getGame, getPlayer, setScore} from "@/lib/queries";
-import {notFound} from "next/navigation";
-import {Player, ScoreRes} from "@/lib/types";
+import { getGame, getPlayer, setScore } from "@/lib/queries";
+import { notFound } from "next/navigation";
+import { Player, ScoreRes } from "@/lib/types";
 
 export const dynamic = 'force-dynamic' // defaults to auto
-export async function POST(request: Request, {params}: { params: { id: string } }) {
+export async function POST(request: Request, { params }: { params: { id: string } }) {
     const game = await getGame(params.id, undefined, true)
     if (!game) return notFound()
     const body = await request.json()
-    if (!body.playerId || body.playerId === "" || !body.scorer || body.scorer === "" || body.scorer !== "model" || body.scorer !== "player") return new Response("Bad request", {
+    if (!body.playerId || body.playerId === "" || !body.scorer || body.scorer === "" || body.scorer !== "model" && body.scorer !== "player") return new Response("Bad request", {
         status: 400
     })
     const player = await getPlayer(body.playerId)
